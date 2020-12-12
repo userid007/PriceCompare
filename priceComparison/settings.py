d9445fce425fe8d6cbbbc11e73698218484653b2
userid007 <shindeakshay7499@gmail.com>
2020-12-13 05:18:35 +0530

    heroku url added

--- priceComparison/settings.py
+++ priceComparison/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '_!+1jz7zykzm1dp*(52zc_x=-&235+%t=))pwk894se8$lu7oy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1"]
 
 # Application definition
 
