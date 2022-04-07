8f7b4992a66351683b7800aca157b44f2c2caf48
userid007 <shindeakshay7499@gmail.com>
2022-04-07 18:38:15 +0530

    amazon bug fixed

--- compare/price_comp.py
+++ compare/price_comp.py
@@ -16,7 +16,8 @@ def flipkart(name):
     try:
         # global flipkart
         name1 = name.replace(" ", "+")  # iphone x  -> iphone+x
-        res = requests.get(f'https://www.flipkart.com/search?q={name1}', headers=headers)
+        res = requests.get(
+            f'https://www.flipkart.com/search?q={name1}', headers=headers)
 
         print("\nSearching in flipkart....")
         soup = BeautifulSoup(res.text, 'html.parser')
@@ -59,7 +60,8 @@ def flipkart(name):
                 if name.upper() in title:
                     # New Class For Product Price
                     price = soup.select('._30jeq3')[i].getText().strip()
-                    title = soup.select('._4rR01T')[i].getText().strip().upper()
+                    title = soup.select('._4rR01T')[
+                        i].getText().strip().upper()
                     url += soup.select('._1fQZEK')[i]['href'].strip()
                     image = soup.select('._396cs4')[i + 1]['src'].strip()
                     if image.find("placeholder_fcebae.svg") != -1:
@@ -89,7 +91,8 @@ def flipkart(name):
         title = "No product found"
         price = '0'
 
-    product = {"title": title, "price": price, "url": url, "image": image, "site": "Flipkart"}
+    product = {"title": title, "price": price,
+               "url": url, "image": image, "site": "Flipkart"}
     return product
 
 
@@ -102,22 +105,30 @@ def amazon(name):
         # global amazon
         name1 = name.replace(" ", "-")
         name2 = name.replace(" ", "+")
-        res = requests.get(f'https://www.amazon.in/{name1}/s?k={name2}', headers=headers)
+        # res = requests.get(f'https://www.amazon.in/{name1}/s?k={name2}', headers=headers)
+        res = requests.get(
+            f'https://www.amazon.in/s?k={name2}', headers=headers)
+
         print("\nSearching in amazon:")
         soup = BeautifulSoup(res.text, 'html.parser')
-        title_list = soup.select('.a-color-base.a-text-normal')
-        # print(title_list)
+        title_list = soup.select('.a-size-medium.a-color-base.a-text-normal')
+        # print(link_list)
         amazon_page_length = int(len(title_list))
-        # print(amazon_page_length)
+        # print(len(amazon_page_length))
         for i in range(0, amazon_page_length):
             name = name.upper()
             title = soup.select(
                 '.a-color-base.a-text-normal')[i].getText().strip().upper()
             if name in title:
-                title = soup.select('.a-color-base.a-text-normal')[i].getText().strip().upper()
-                price = "₹" + soup.select('.a-price-whole')[i].getText().strip()
-                url += soup.select(".a-link-normal.a-text-normal.s-no-hover.a-text-normal")[i]['href'].strip()
-                image = soup.select("div.a-section.aok-relative.s-image-fixed-height img")[i]['src'].strip()
+                title = soup.select(
+                    '.a-color-base.a-text-normal')[i].getText().strip().upper()
+                price = "₹" + \
+                    soup.select('.a-price-whole')[i].getText().strip()
+                url += soup.select(
+                    ".a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal")[i*2]['href'].strip()
+                print(url)
+                image = soup.select(
+                    "div.a-section.aok-relative.s-image-fixed-height img")[i]['src'].strip()
                 print("Amazon:")
                 print(title)
                 print(price)
@@ -134,14 +145,16 @@ def amazon(name):
                     title = "No product found"
                     price = '-'
                     break
-        product = {"title": title, "price": price, "url": url, "image": image, "site": "Amazon"}
+        product = {"title": title, "price": price,
+                   "url": url, "image": image, "site": "Amazon"}
         return product
     except:
         print("amazon: No product found!")
         print("-----------------------")
         title = "No product found"
         price = '-'
-    product = {"title": title, "price": price, "url": url, "image": image, "site": "Amazon"}
+    product = {"title": title, "price": price,
+               "url": url, "image": image, "site": "Amazon"}
     return product
 
 
